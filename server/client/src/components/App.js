7616de95b6dfc7fa2a2cb4d48a74c9024e15e5fc
sisi li <alicesqcg@gmail.com>
2018-06-29 08:16:09 +0800

    updated issuePage

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import HomePage from './HomePage';
+import NewIssue from './NewIssue';
 
 
 
 const IssueNew = () => <h2>IssueNew</h2>
 
-const App = () => {
-    return (
-        <div>
-           <BrowserRouter>
-              <div>
-                <Header /> 
-                <Route exact path="/" component={HomePage} />
-                <Route path="/issues" component={IssueNew} />
-              </div>
-           </BrowserRouter>
-        </div>
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+            <BrowserRouter>
+                <div>
+                    <Header /> 
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/issues" component={NewIssue} />
+                </div>
+            </BrowserRouter>
+            </div>
 
-    );
+        );
+    }
 };
 
 export default App;
---
+++ server/client/src/components/NewIssue.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewIssue extends Component {
+    render() {
+        return (
+            <div>
+                this page is for adding issues
+            </div>
+        );
+  
+    }
+  }
+  
+  export default NewIssue;
