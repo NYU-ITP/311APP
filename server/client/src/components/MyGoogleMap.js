be55316b6cd6ba07bd3667afc503619cc08db7c5
sisi li <alicesqcg@gmail.com>
2018-07-04 06:26:20 +0800

    added marker cluster

--- server/client/src/components/MyGoogleMap.js
+++ server/client/src/components/MyGoogleMap.js
@@ -1,48 +1,80 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-import React, { Component } from 'react';
+import React from 'react';
+const fetch = require("isomorphic-fetch");
+const { compose, withProps, withHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} = require("react-google-maps");
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
 const testLoc = [
-  { latitude: 33.8688, longitude: 151.2093},
-  { latitude: 34.4278, longitude: 150.8931},
-  { latitude: 34.4792, longitude: 150.4181},
-  { latitude: 34.6738, longitude: 150.8444},
-  { latitude: 34.8833, longitude: 150.6000},
-  { latitude: 34.7479, longitude: 149.7277},
+  { latitude: -33.8688, longitude: 151.2093},
+  { latitude: -34.4278, longitude: 150.8931},
+  { latitude: -34.4792, longitude: 150.4181},
+  { latitude: -34.6738, longitude: 150.8444},
+  { latitude: -34.8833, longitude: 150.6000},
+  { latitude: -34.7479, longitude: 149.7277},
 
 ];
 
-
-class MyGoogleMap extends Component {
-  state = {
-    locations: testLoc
-  }
-
-  render () {
-    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MapWithAMarkerClusterer = compose(
+  withProps({
+    googleMapURL:"https://maps.googleapis.com/maps/api/js?key=AIzaSyDjFZnvXXlS5OXSbKSpLRSD-c6dFdsplo4&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      const clickedMarkers = markerClusterer.getMarkers()
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+      console.log(clickedMarkers)
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
-    <Marker 
-      position = {{lat: -34.397, lng: 150.644 }}
-    />
-    {this.state.locations.map(location => (
+    <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.markers.map(marker => (
         <Marker
-          position={{ lat: location.latitude, lng: location.longitude }}
+          position={{ lat: marker.latitude, lng: marker.longitude }}
         />
       ))}
-
+    </MarkerClusterer>
   </GoogleMap>
-))
+);
+
+
+
+class MyGoogleMap extends React.PureComponent {
+  componentWillMount() {
+    this.setState({ markers: testLoc })
+  }
+
+  render () {
     return (
       <div>
-        <MyMapComponent isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjFZnvXXlS5OXSbKSpLRSD-c6dFdsplo4&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-             />
+      <MapWithAMarkerClusterer markers={this.state.markers} />
+      <ul>
+                {this.state.markers.map(marker =>
+                <li>
+                    <h2>{marker.latitude}</h2>
+                    <p>{marker.longitude}</p>
+                </li>)}
+              </ul>
       </div>
+      
     );
   }
 }
