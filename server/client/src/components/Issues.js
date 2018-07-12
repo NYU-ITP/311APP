526ef3491c6bde1e32d03d7dd8a7a03d6a89e39e
xiaozdezhu <06angelina19@gmail.com>
2018-07-12 14:33:50 -0400

    update

--- server/client/src/components/Issues.js
+++ server/client/src/components/Issues.js
@@ -11,7 +11,7 @@ class Issues extends Component {
   };
 
   state = {
-    issues: [{"issueId":1,"time":"2000-01-01T05:00:00.000Z","heading":"Trash disposal","category":"Garbage storage","content":"Property owners must clean and sweep the sidewalks and gutters next to their property, including 18 inches from the curb into the street. Property owners who do not clean the sidewalks and gutters bordering their property may be issued a summons.","location":"Washington Square","urgent":1,"downvote":1,"upvote":3},{"issueId":2,"time":"1998-02-02T05:00:00.000Z","heading":"Damaged tree","category":"Streets and sidewalks","content":"The Department of Parks and Recreation (DPR) removes street trees and large branches that have fallen to the ground in front of houses, in parks, and in other public places. ","location":"Wasserman Center","urgent":0,"downvote":null,"upvote":2}]
+    issues: []
   }
 
   componentDidMount() {
