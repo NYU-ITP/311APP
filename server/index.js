cae7be0be5681661738c5aa90760ef36f7f64e6d
xiaozdezhu <06angelina19@gmail.com>
2018-07-27 18:40:56 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -289,7 +289,7 @@ app.post('/api/issuesMun', jsonParser, (req, res) => {
   });
 });
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file
   app.use(express.static(client/build));
